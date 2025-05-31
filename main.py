a3fa01ebed12adf23ab047d96a3091cbf5934f2f
Dervanius <bristanovic@hotmail.com>
2025-05-31 17:06:23 +0200

    Completed version

--- main.py
+++ main.py
@@ -30,12 +30,6 @@ resources = {
     "coffee": 100,
 }
 
-# print(MENU["espresso"]["ingredients"]["water"])
-
-# print(MENU["espresso"]["ingredients"])
-
-# for x in MENU["espresso"]["ingredients"]:
-#     print(x)
 
 def check_resources(drink, material):
     for substance in MENU[drink]["ingredients"]:
@@ -45,6 +39,7 @@ def check_resources(drink, material):
             return False
     print(f"One {drink} coming right up!\nShow me the money!")
     return True
+
 
 def deduct_resources(drink, material):
     for substance in MENU[drink]["ingredients"]:
@@ -56,23 +51,26 @@ def initial_selection():
     selection = input("What would you like? (espresso/latte/cappuccino)\n").lower()
     return selection
 
+
 def status():
-    print(f"Milk: {resources["milk"]}")
-    print(f"Water: {resources["water"]}")
-    print(f"Coffee: {resources["coffee"]}")
-    print(f"House score: {house_score}")
+    print(f"Milk: {resources["milk"]} ml")
+    print(f"Water: {resources["water"]} ml")
+    print(f"Coffee: {resources["coffee"]} g")
+    print(f"House score: ${house_score}")
+
 
 def payment(unit, value, paid):
-    bane = int((input(f"How manu {unit}? "))) * value
-    paid += bane
-    print(f"Money paid: {round(paid, 2) }")
+    balance = int((input(f"How manu {unit}? "))) * value
+    paid = balance
+    return round(paid,2)
+
 
 is_machine_on = True
-
+house_score = 0
 
 while is_machine_on:
     working_mode = True
-    house_score = 0
+
     while working_mode:
         choice = initial_selection()
 
@@ -87,22 +85,25 @@ while is_machine_on:
                 working_mode = False
             else:
                 customer_score = 0
-                payment("quarters", 0.25, customer_score)
-                payment("dimes", 0.10, customer_score)
-                payment("nickles", 0.05, customer_score)
-                payment("pennies", 0.01, customer_score)
-
+                customer_score += payment("quarters", 0.25, customer_score)
+                print(f"Money paid: ${round(customer_score, 2)}")
+                customer_score += payment("dimes", 0.10, customer_score)
+                print(f"Money paid: ${round(customer_score, 2)}")
+                customer_score += payment("nickles", 0.05, customer_score)
+                print(f"Money paid: ${round(customer_score, 2)}")
+                customer_score += payment("pennies", 0.01, customer_score)
+                print(f"Money paid: ${round(customer_score, 2)}")
+                # house_score += customer_score
 
                 if MENU[choice]["cost"] < customer_score:
                     calculate_change = customer_score - MENU[choice]["cost"]
                     print(f"Here is your change: ${calculate_change}")
                     customer_score -= calculate_change
-                    house_score = customer_score
-                    print(f"Money paid: {customer_score}")
+                    print(f"Money paid: ${customer_score}")
 
                 if MENU[choice]["cost"] == customer_score:
-                    house_score += customer_score;
-                    print(f"You've {customer_score} paid and you deserve {choice}")
+                    house_score += customer_score
+                    print(f"You've ${customer_score} paid and you deserve {choice}")
                     deduct_resources(choice, resources)
                 else:
                     print("You are too poor. Next...")
